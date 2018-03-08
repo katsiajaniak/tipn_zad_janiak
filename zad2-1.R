5f16b9feb94bc47ac4262a13ce1c6bc69d3c1888
katsiajaniak <37178871+katsiajaniak@users.noreply.github.com>
2018-03-08 15:35:00 +0100

    Zadania

---
+++ zad2-1.R
@@ -0,0 +1,12 @@
+kwartal_1 <- c("Styczeń", "Luty", "Marzec")
+kwartal_2 <- c("Kwiecień", "Maj", "Czerwiec")
+kwartal_3 <- c("Lipiec", "Sierpień", "Wrzesień")
+kwartal_4 <- c("Październik", "Listopad", "Grudzień")
+rok <- c(kwartal_1, kwartal_2, kwartal_3, kwartal_4)
+rok [1:2]
+rok [8:12]
+rok [c(1,3,5,7,9,11)]
+nchar (rok)
+mean (nchar(rok))
+sd(nchar(rok))
+zima <-  c("grudzień", "luty", "marzec")
